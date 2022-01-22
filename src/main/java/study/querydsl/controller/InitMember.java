cdfafe1503717d0c8346396703e91f69b4913e50
IcedAmeri <seokhyunkho@gmail.com>
2022-01-23 06:27:10 +0900

    [실무 활용 - 스프링 데이터 JPA와 Querydsl] 스프링 데이터 페이징 활용3 - 컨트롤러 개발

--- src/main/java/study/querydsl/controller/InitMember.java
+++ src/main/java/study/querydsl/controller/InitMember.java
@@ -36,7 +36,7 @@ public class InitMember {
             em.persist(teamA);
             em.persist(teamB);
 
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 99; i++) {
                 Team selectedTeam = i % 2 == 0 ? teamA : teamB;
                 em.persist(new Member("member" + i, i, selectedTeam));
             }
--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -10,5 +10,4 @@ import java.util.List;
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
     Page<MemberTeamDto> searchPage(MemberSearchCondition condition, Pageable pageable);
-
 }
